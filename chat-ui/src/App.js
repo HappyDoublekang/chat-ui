b3ad24919720fdfa5dfc4fb41b4ea7b1d01de314
SaberArthur <467455219@qq.com>
2021-07-17 09:21:45 +0800

    feat：通过useState动态修改参数

--- chat-ui/src/App.js
+++ chat-ui/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from "./Button"
+import { useState } from 'react';
 
 function App() {
+  const [color, setColor] = useState("#ff0000");
+
   const handleButton1Click = () => {
     console.log("点击事件1");
+    setColor("#00ff00");
   }
   const handleButton2Click = () => {
     console.log("点击事件2");
@@ -16,6 +20,7 @@ function App() {
           &gt;
         </span>
       </Button>
+      <p style={{color: color}}>这是一段文字</p>
       <Button onClick={handleButton2Click} label="点我" />
     </div>
   );
