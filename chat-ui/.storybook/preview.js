11c2b4e09e095daa499e241f693fe4cf0b3cffb2
SaberArthur <467455219@qq.com>
2021-07-19 10:02:33 +0800

    feat：配置storybook的preview.js

--- chat-ui/.storybook/preview.js
+++ chat-ui/.storybook/preview.js
@@ -1,9 +1,10 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import React from "react"
+import { addDecorator } from "@storybook/react"
+import { ThemeProvider } from "styled-components"
+import theme from "../src/theme"
+
+addDecorator((storyFn) => {
+  <ThemeProvider theme={theme}>
+    {storyFn()}
+  </ThemeProvider>
+});
