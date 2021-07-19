cc4007371a8b3b11a7ba5e57f4efec2b4d03a7c0
SaberArthur <467455219@qq.com>
2021-07-19 14:44:31 +0800

    feat：添加图片

---
+++ chat-ui/src/components/Avatar/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import face1 from ''
+
+function Avatar(props) {
+  return (
+    <div>
+      <div>111</div>
+      <div>
+        <img src={face1} alt=""></img>
+      </div>
+    </div>
+  )
+}
+
+Avatar.propTypes = {}
+
+export default Avatar
+
